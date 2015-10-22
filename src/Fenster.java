c191731e1905d776b85d0be57b6397281e0cf2f6
emkoo <elmir.tanjo@mail.de>
2015-10-23 00:42:28 +0200

    Hallo

---
+++ src/Fenster.java
@@ -0,0 +1,31 @@
+import java.awt.FlowLayout;
+
+import java.awt.*;
+import javax.swing.*;
+//import javax.swing.JPanel;
+
+public class Fenster extends JFrame {
+	
+	JLabel label;
+	JPanel panel;
+	
+	Fenster(String titel){
+		super(titel);
+		setSize(480, 480);
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		
+		setLayout(new FlowLayout());
+		label = new JLabel("Hallo Welt");
+		add(label);
+		
+		
+	}
+	
+	public static void main(String[] args){
+		
+		Fenster fenster =new Fenster("Test");
+		fenster.setVisible(true);
+		
+	}
+
+}
