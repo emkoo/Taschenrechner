5661f4cc7de0ce84c6a29616969d163a1b1cefe5
emkoo <elmir.tanjo@mail.de>
2015-11-05 22:49:01 +0100

    Test

--- src/Fenster.java
+++ src/Fenster.java
@@ -1,33 +1,39 @@
-import java.awt.FlowLayout;
-
 import java.awt.*;
 import javax.swing.*;
 //import javax.swing.JPanel;
 
 public class Fenster extends JFrame {
-	
+
 	JLabel label;
-	JPanel panel;
-	
-	Fenster(String titel){
+	JPanel panel, panel2;
+	String[] zahlen = { "7", "8", "9", "4", "5", "6", "1", "2", "3", "0" };
+
+	Fenster(String titel) {
 		super(titel);
-		setSize(480, 480);
+		setSize(240, 240);
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setLayout(new BorderLayout());
+		panel = new JPanel();
+		panel2 = new JPanel();
+		add(panel, BorderLayout.SOUTH);
 		
-		setLayout(new FlowLayout());
-		label = new JLabel("Hallo Welt");
-		add(label);
-		
-		
+		panel.setLayout(new BorderLayout());
+		panel2.setLayout(new GridLayout(4, 3, 3, 3));
+		panel.add(panel2, BorderLayout.WEST);
+		JButton[] button = new JButton[zahlen.length];
+		for (int i = 0; i < zahlen.length; i++) {
+			button[i] = new JButton(zahlen[i]);
+			panel2.add(button[i]);
+
+		}
+
 	}
-	
-	public static void main(String[] args){
-		
-		Fenster fenster =new Fenster("Test");
-		Operationen op = new Operationen();
-		System.out.println(op.add(2, 3));
+
+	public static void main(String[] args) {
+
+		Fenster fenster = new Fenster("Taschenrechner");
 		fenster.setVisible(true);
-		
+
 	}
 
 }
